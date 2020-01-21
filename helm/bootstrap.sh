24669f246e6bb1d525d184992f7bd5d3bb80b96d
wgentry22 <william.gentry02@gmail.com>
2020-01-21 08:06:00 -0500

    Fixed username availability bug

--- helm/bootstrap.sh
+++ helm/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Create application namespace
-kubectl create namespace ers && echo "Namespace 'ers' created"
+kubectl create namespace ers
 
 # Create (Cluster) Role, (Cluster) Role Binding, and Service Account for Tiller
 kubectl apply -f .tiller/
@@ -20,7 +20,7 @@ minikube addons enable metrics-server
 # Create kube-state-metrics server in kube-system namespace
 kubectl apply -f https://raw.githubusercontent.com/gjeanmart/kauri-content/master/spring-boot-simple/k8s/kube-state-metrics.yml
 
-# Build Application Containers
+# Build Application Container Images
 docker build -t ers-server ../server/
 docker build -t ers-client ../client/
 
--- helm/build-containers.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd ../server/ || exit 1
-docker build -t ers-server .
-cd ../client/ || exit 1
-docker build -t ers-client .
-cd ../helm/
